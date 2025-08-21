e55601cbe29b46084bf5a979c5a2c173f29621f9
B. Szonye <bszonye@gmail.com>
2025-08-20 23:27:46 -0700

    fallbacks for selected city

--- ui/lenses/layer/bz-urban-layer.js
+++ ui/lenses/layer/bz-urban-layer.js
@@ -1,6 +1,8 @@
 import { L as LensManager } from '/core/ui/lenses/lens-manager.chunk.js';
 import { O as OVERLAY_PRIORITY } from '/base-standard/ui/utilities/utilities-overlay.chunk.js';
 import { U as UpdateGate } from '/core/ui/utilities/utilities-update-gate.chunk.js';
+import { BuildingPlacementManager } from '/base-standard/ui/building-placement/building-placement-manager.js';
+import PlotWorkersManager from '/base-standard/ui/plot-workers/plot-workers-manager.js';
 // make sure the city lenses load first
 import '/base-standard/ui/lenses/lens/acquire-tile-lens.js';
 import '/base-standard/ui/lenses/lens/building-placement-lens.js';
@@ -24,7 +26,9 @@ class bzUrbanLayer {
         this.urbanOverlay.clear();
         this.centerOverlay.clear();
         // update city overlays
-        const cityID = UI.Player.getHeadSelectedCity();
+        const cityID = UI.Player.getHeadSelectedCity() ??
+            BuildingPlacementManager.cityID ??
+            PlotWorkersManager.cityID;
         const city = Cities.get(cityID);
         if (!city) return;
         const centerPlot = GameplayMap.getIndexFromLocation(city.location);
