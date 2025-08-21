10617a1d9afb0bfef0e3a225e70af9d4e5e9247c
B. Szonye <bszonye@gmail.com>
2025-08-21 02:42:01 -0700

    restore city-details panel

--- ui/production-chooser/bz-panel-production-chooser.js
+++ ui/production-chooser/bz-panel-production-chooser.js
@@ -4,6 +4,15 @@
 // - remember Production/Purchase tab selection
 const BZ_HEAD_STYLE = [
 `
+.bz-city-hall .panel-production__frame {
+    min-width: 28.4444444444rem;
+    max-width: 28.4444444444rem;
+}
+.bz-city-hall .production-chooser__city-details-button {
+    position: fixed;
+    top: 4rem;
+    right: 2rem;
+}
 .bz-city-hall .advisor-recommendation__container .advisor-recommendation__icon {
     width: 1.1111111111rem;
     height: 1.1111111111rem;
@@ -17,97 +26,96 @@ BZ_HEAD_STYLE.map(style => {
 });
 document.body.classList.add("bz-city-hall");
 export class bzProductionChooserScreen {
-    static panel_prototype;
-    static panel_doOrConfirmConstruction;
+    static c_prototype;
     static isPurchase = false;
-    static isCDPanelOpen = true;
-    constructor(panel) {
-        this.panel = panel;
-        panel.bzPanel = this;
-        this.patchPrototypes(this.panel);
+    static isCDPanelOpen = false;
+    constructor(component) {
+        this.component = component;
+        component.bzComponent = this;
+        this.patchPrototypes(this.component);
     }
-    patchPrototypes(panel) {
-        const panel_prototype = Object.getPrototypeOf(panel);
-        if (bzProductionChooserScreen.panel_prototype == panel_prototype) return;
+    patchPrototypes(component) {
+        const c_prototype = Object.getPrototypeOf(component);
+        if (bzProductionChooserScreen.c_prototype == c_prototype) return;
         // patch PanelCityDetails methods
-        const proto = bzProductionChooserScreen.panel_prototype = panel_prototype;
-        // override doOrConfirmConstruction method
-        bzProductionChooserScreen.panel_doOrConfirmConstruction =
-            proto.doOrConfirmConstruction;
+        const proto = bzProductionChooserScreen.c_prototype = c_prototype;
         // override isPurchase property
-        const panel_isPurchase =
-            Object.getOwnPropertyDescriptor(panel_prototype, "isPurchase");
+        const c_isPurchase =
+            Object.getOwnPropertyDescriptor(proto, "isPurchase");
         const isPurchase = {
-            configurable: panel_isPurchase.configurable,
-            enumerable: panel_isPurchase.enumerable,
-            get: panel_isPurchase.get,
+            configurable: c_isPurchase.configurable,
+            enumerable: c_isPurchase.enumerable,
+            get: c_isPurchase.get,
             set(value) {
                 // remember tab selection
                 bzProductionChooserScreen.isPurchase = value;
-                panel_isPurchase.set.apply(this, [value]);
+                c_isPurchase.set.apply(this, [value]);
             },
         };
-        Object.defineProperty(panel_prototype, "isPurchase", isPurchase);
+        Object.defineProperty(proto, "isPurchase", isPurchase);
         // override cityID property
-        const panel_cityID =
-            Object.getOwnPropertyDescriptor(panel_prototype, "cityID");
+        const c_cityID =
+            Object.getOwnPropertyDescriptor(proto, "cityID");
         const cityID = {
-            configurable: panel_cityID.configurable,
-            enumerable: panel_cityID.enumerable,
-            get: panel_cityID.get,
+            configurable: c_cityID.configurable,
+            enumerable: c_cityID.enumerable,
+            get: c_cityID.get,
             set(value) {
-                panel_cityID.set.apply(this, [value]);
+                c_cityID.set.apply(this, [value]);
                 // restore tab selection (if needed & possible)
                 if (this._isPurchase || this.city.Happiness?.hasUnrest) return;
                 if (bzProductionChooserScreen.isPurchase) this.isPurchase = true;
             },
         };
-        Object.defineProperty(panel_prototype, "cityID", cityID);
+        Object.defineProperty(proto, "cityID", cityID);
         // override items property
-        const panel_items =
-            Object.getOwnPropertyDescriptor(panel_prototype, "items");
+        const c_items =
+            Object.getOwnPropertyDescriptor(proto, "items");
         const items = {
-            configurable: panel_items.configurable,
-            enumerable: panel_items.enumerable,
-            get: panel_items.get,
+            configurable: c_items.configurable,
+            enumerable: c_items.enumerable,
+            get: c_items.get,
             set(value) {
                 // sort items
                 for (const [_key, list] of Object.entries(value)) {
                     list.sort((a, b) => {
+                        // TODO: implement .sortvalue and .sortCost
                         // sort by value (higher absolute value is better)
-                        if (a.sortValue != b.sortValue) {
-                            if (a.sortValue < 0 || b.sortValue < 0) {
-                                // negative values sort first (repairs & civilians)
-                                return a.sortValue - b.sortValue;
-                            }
-                            return b.sortValue - a.sortValue;
+                        const aValue = a.sortValue ?? 0;
+                        const bValue = b.sortValue ?? 0;
+                        if (aValue != bValue) {
+                            // negative values sort first (repairs & civilians)
+                            const dir = aValue < 0 || bValue < 0 ? -1 : +1;
+                            return dir * (bValue - aValue);
                         }
                         // sort by cost (lower is better)
-                        if (a.sortCost != b.sortCost) return a.sortCost - b.sortCost;
+                        const aCost = a.sortCost ?? 0;
+                        const bCost = b.sortCost ?? 0;
+                        if (aCost != bCost) return aCost - bCost;
                         // finally, sort by name
                         const aName = Locale.compose(a.name);
                         const bName = Locale.compose(b.name);
                         return aName.localeCompare(bName);
                     });
                 }
-                panel_items.set.apply(this, [value]);
+                c_items.set.apply(this, [value]);
             },
         };
-        Object.defineProperty(panel_prototype, "items", items);
+        Object.defineProperty(proto, "items", items);
     }
     beforeAttach() { }
     afterAttach() {
-        engine.on('ConstructibleChanged', this.panel.onConstructibleAddedToMap, this.panel);
+        engine.on('ConstructibleChanged', this.component.onConstructibleAddedToMap, this.component);
         // restore the city details panel if it was open previously
-        if (bzProductionChooserScreen.isCDPanelOpen && !this.panel.isSmallScreen()) {
-            this.panel.showCityDetails();
+        if (bzProductionChooserScreen.isCDPanelOpen && !this.component.isSmallScreen()) {
+            this.component.showCityDetails();
         }
     }
     onAttributeChanged(_name, _prev, _next) { }
     beforeDetach() {
-        if (!this.panel.isSmallScreen()) {
+        if (!this.component.isSmallScreen()) {
             // remember whether the city details panel is open
-            const cdSlot = this.panel.cityDetailsSlot;
+            const cdSlot = this.component.cityDetailsSlot;
             const cdPanel = cdSlot?.querySelector(".panel-city-details");
             bzProductionChooserScreen.isCDPanelOpen =
                 cdPanel && !cdPanel.classList.contains("hidden");
@@ -118,7 +126,7 @@ export class bzProductionChooserScreen {
         // this includes switches to the building-placement interface,
         // but that has its own means of restoring the Purchase tab.
         bzProductionChooserScreen.isPurchase = false;
-        engine.off('ConstructibleChanged', this.panel.onConstructibleAddedToMap, this.panel);
+        engine.off('ConstructibleChanged', this.component.onConstructibleAddedToMap, this.component);
     }
 }
 Controls.decorate('panel-production-chooser', (val) => new bzProductionChooserScreen(val));
