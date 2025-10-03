e6d6f05a94829ba9be576e44c4326e68037ec1b7
B. Szonye <bszonye@gmail.com>
2025-10-02 17:56:30 -0700

    improved building slot display

--- ui/lenses/layer/bz-building-placement-layer.js
+++ ui/lenses/layer/bz-building-placement-layer.js
@@ -1,16 +1,53 @@
 import { L as LensManager } from '/core/ui/lenses/lens-manager.chunk.js';
+// import { Y as YieldChangeVisualizer } from '/base-standard/ui/lenses/layer/yield-change-visualizer.chunk.js';
 import { realizeBuildSlots } from '/bz-city-hall/ui/lenses/layer/bz-building-slots.js';
 // make sure the vanilla layer loads first
 import '/base-standard/ui/lenses/layer/building-placement-layer.js';
 
 // get registered lens layer
-// TODO: determine whether any of this is still necessary
-const _WYLL = LensManager.layers.get("fxs-worker-yields-layer");
-const WYLL = {};
+const BPL = LensManager.layers.get("fxs-building-placement-layer");
 
-// switch to reusable method (shared with other city screens)
-WYLL.YIELD_SPRITE_HEIGHT = 6;
-WYLL.YIELD_SPRITE_ANGLE = Math.PI / 6;  // 30°
-WYLL.realizeBuildSlots = function(district) {
-    return realizeBuildSlots.apply(this, [district, this.yieldSpriteGrid]);
+// remove vanilla sprite grids
+// BPL.yieldVisualizer.release();
+// BPL.adjacenciesSpriteGrid.destroy();
+// create new grids for building slots and yields
+BPL.slotGrid = WorldUI.createSpriteGrid(
+    "bzBPLSlot_SpriteGroup",
+    SpriteMode.Billboard
+);
+BPL.yieldGrid = WorldUI.createSpriteGrid(
+    "bzBPLSlot_SpriteGroup",
+    SpriteMode.Billboard
+);
+// recreate vanilla sprite grids for proper layering
+// BPL.yieldVisualizer = new YieldChangeVisualizer("BuildingPlacement");
+BPL.adjacenciesSpriteGrid = WorldUI.createSpriteGrid(
+    "Adjacencies_SpriteGroup",
+    SpriteMode.Default
+);
+// modify build slot rendering
+BPL.buildSlotAngle = Math.PI / 6;  // 30°
+BPL.realizeBuildSlots = function(district) {
+    const args = [district, this.slotGrid, this.yieldGrid];
+    return realizeBuildSlots.apply(this, args);
+}
+const BPL_initLayer = BPL.initLayer;
+BPL.initLayer = function(...args) {
+    BPL.slotGrid.setVisible(false);
+    BPL.yieldGrid.setVisible(false);
+    return BPL_initLayer.apply(this, args);
+}
+const BPL_applyLayer = BPL.applyLayer;
+BPL.applyLayer = function(...args) {
+    BPL.slotGrid.setVisible(true);
+    BPL.yieldGrid.setVisible(true);
+    return BPL_applyLayer.apply(this, args);
+}
+const BPL_removeLayer = BPL.removeLayer;
+BPL.removeLayer = function(...args) {
+    BPL.slotGrid.clear(false);
+    BPL.slotGrid.setVisible(false);
+    BPL.yieldGrid.clear(false);
+    BPL.yieldGrid.setVisible(false);
+    return BPL_removeLayer.apply(this, args);
 }
--- ui/lenses/layer/bz-building-slots.js
+++ ui/lenses/layer/bz-building-slots.js
@@ -1,13 +1,15 @@
 import { C as ComponentID } from '/core/ui/utilities/utilities-component-id.chunk.js';
 
-function adjacencyYield(building) {
+function getYields(building) {
     if (!building) return [];
-    const adjTypes = GameInfo.Constructible_Adjacencies.filter(at =>
-        at.ConstructibleType == building.ConstructibleType && !at.RequiresActivation
-    );
-    const adjYields = adjTypes.map(at => GameInfo.Adjacency_YieldChanges.find(
-        ay => ay.ID == at.YieldChangeId));
-    const yieldSet = new Set(adjYields.map(ay => ay.YieldType));
+    const type = Game.getHash(building.ConstructibleType);
+    const base = GameInfo.Constructible_YieldChanges.filter(y => y.$hash == type);
+    base.sort((a, b) => b.YieldChange - a.YieldChange);
+    const atypes = GameInfo.Constructible_Adjacencies
+        .filter(a => a.$hash == type && !a.RequiresActivation)
+        .map(a => Game.getHash(a.YieldChangeId));
+    const bonus = atypes.map(atype => GameInfo.Adjacency_YieldChanges.lookup(atype));
+    const yieldSet = new Set([...base, ...bonus].map(y => y.YieldType));
     return [...yieldSet];
 }
 function gatherBuildingsTagged(tag) {
@@ -15,8 +17,8 @@ function gatherBuildingsTagged(tag) {
 }
 const BZ_LARGE = gatherBuildingsTagged("FULL_TILE");
 
-function realizeBuildSlots(district, grid) {
-    if (!district || !grid) return;
+function realizeBuildSlots(district, slotGrid, yieldGrid) {
+    if (!district || !slotGrid) return;
     const districtDefinition = GameInfo.Districts.lookup(district.type);
     if (!districtDefinition) {
         console.error(
@@ -24,10 +26,8 @@ function realizeBuildSlots(district, grid) {
         );
         return;
     }
-    const constructibles = MapConstructibles.getConstructibles(
-        district.location.x,
-        district.location.y
-    );
+    const loc = district.location;
+    const constructibles = MapConstructibles.getConstructibles(loc.x, loc.y);
     const buildingSlots = [];
     let maxSlots = districtDefinition.MaxConstructibles;
     for (const constructibleID of constructibles) {
@@ -38,47 +38,50 @@ function realizeBuildSlots(district, grid) {
             );
             continue;
         }
-        const building = GameInfo.Constructibles.lookup(
-            existingConstructible.type
-        );
-        if (!building) {
-            console.error("building-placement-layer: Unable to find a valid ConstructibleDefinition with type: " + existingConstructible.type);
+        const info = GameInfo.Constructibles.lookup(existingConstructible.type);
+        if (!info) {
+            console.error(
+                "building-placement-layer: Unable to find a valid ConstructibleDefinition with type: " + existingConstructible.type
+            );
             continue;
         }
         // skip walls
-        if (building.ExistingDistrictOnly) continue;
+        if (info.ExistingDistrictOnly) continue;
         // large buildings take up an extra slot
-        if (BZ_LARGE.has(building.ConstructibleType)) maxSlots -= 1;
+        if (BZ_LARGE.has(info.ConstructibleType)) maxSlots -= 1;
         // building icon
-        const iconURL = UI.getIconBLP(building.ConstructibleType) || "";
+        const iconURL = UI.getIconBLP(info.ConstructibleType) || "";
         // building yield type flag
-        const yields = adjacencyYield(building)
-            .map(y => getYieldFlag(y));
+        const yields = getYields(info).map(y => UI.getIconBLP(y + "_5"));
         buildingSlots.push({ iconURL, yields, });
     }
+    const origin = this.buildSlotSpritePosition ?? { x: 0, y: 24, z: 0 };
+    const scale = this.buildSlotSpriteScale ?? 0.9;
+    const padding = this.buildSlotSpritePadding;
     for (let i = 0; i < maxSlots; i++) {
-        const groupWidth = (maxSlots - 1) * this.BUILD_SLOT_SPRITE_PADDING;
-        const xPos = (i * this.BUILD_SLOT_SPRITE_PADDING) + (groupWidth / 2) - groupWidth;
-        grid.addSprite(district.location, UI.getIconBLP('BUILDING_UNFILLED'), { x: xPos, y: -28, z: 0 });
+        const groupWidth = (maxSlots - 1) * padding;
         const slot = buildingSlots[i];
-        if (slot) {
-            const p = { x: xPos, y: -27.5, z: 0 };
+        const icon = slot ? slot.iconURL : UI.getIconBLP("BUILDING_EMPTY");
+        const position = { ...origin };
+        position.x = origin.x + i * padding - groupWidth / 2;
+        const params = { scale: slot ? scale : 8/9 * scale };
+        slotGrid.addSprite(loc, icon, position, params);
+        if (slot && yieldGrid) {
+            // TODO: move to bottom of icon
             for (const [j, yieldIcon] of slot.yields.entries()) {
+                const p = { ...position };
                 const w = slot.yields.length - 1;
-                const dx = this.YIELD_SPRITE_PADDING * 2/3 * (j - w/2);
-                const dy = this.YIELD_SPRITE_HEIGHT * Math.cos(this.YIELD_SPRITE_ANGLE);
-                const dz = this.YIELD_SPRITE_HEIGHT * Math.sin(this.YIELD_SPRITE_ANGLE);
-                const pf = { x: p.x + dx, y: p.y + dy, z: p.z + dz };
-                grid.addSprite(district.location, yieldIcon, pf, { scale: 3/4 });
+                const a = (j - w/2) * 2/7 * Math.PI;
+                const dx = 7 * scale * Math.sin(a);
+                const dy = -7 * scale * Math.cos(a);
+                const params = { scale: scale / 2 };
+                p.x += dx;
+                p.y += dy * Math.cos(this.buildSlotAngle);
+                p.z += dy * Math.sin(this.buildSlotAngle);
+                yieldGrid.addSprite(loc, yieldIcon, p, params);
             }
-            grid.addSprite(district.location, slot.iconURL, p, { scale: 0.7 });
         }
     }
-}
-
-function getYieldFlag(yieldType) {
-    if (yieldType == "YIELD_DIPLOMACY") yieldType = "yield_influence";
-    return `${yieldType.toLowerCase()}_pos-lrg`;
 }
 
 export { realizeBuildSlots };
