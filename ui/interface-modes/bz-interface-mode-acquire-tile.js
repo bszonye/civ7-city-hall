8b34dfeb5283102d0d9d3756ebda38506da03e46
B. Szonye <bszonye@gmail.com>
2025-10-05 09:35:41 -0700

    brighter specialist colors

--- ui/interface-modes/bz-interface-mode-acquire-tile.js
+++ ui/interface-modes/bz-interface-mode-acquire-tile.js
@@ -4,13 +4,13 @@ import { C as CityZoomer } from '/base-standard/ui/city-zoomer/city-zoomer.chunk
 import PlotWorkersManager from '/base-standard/ui/plot-workers/plot-workers-manager.js';
 
 const YIELD_COLOR = [
-    0xff4db380,  // #80b34d   90° 40 50 green
-    0xff293da3,  // #a33d29   10° 60 40 red
-    0xff55cef6,  // #f6ce55   45° 90 65 yellow
-    0xffe0a66c,  // #6ca6e0  210° 65 65 cyan
-    0xffd65c5c,  // #5c5cd6  240° 60 60 violet
-    0xff3d99f5,  // #f5993d   30° 90 60 orange
-    0xffcfb7af,  // #afb7cf  225° 25 75 gray
+    0xff30b57d,  // #7db530  #80b34d   90° 40 50 green
+    0xff293da3,  //          #a33d29   10° 60 40 red
+    0xff55cef6,  //          #f6ce55   45° 90 65 yellow
+    0xfffe991d,  // #1d99fe  #6ca6e0  210° 65 65 cyan
+    0xffff7b7c,  // #7c7bff  #5c5cd6  240° 60 60 violet
+    0xff2093fd,  // #fd9320  #f5993d   30° 90 60 orange
+    0xffcfb7af,  //          #afb7cf  225° 25 75 gray
 ];
 
 // get registered interface mode object
@@ -31,8 +31,7 @@ ATIM.decorate = function(overlay) {
     const CITY_TILE_GRAY_COLOR = { x: 0, y: 0, z: 0, w: 0.1 };
     const EXPAND_CITY_COLOR_LINEAR = { x: 0.8, y: 1, z: 0, w: 0.6 };
     const EXPAND_CITY_BORDER_COLOR_LINEAR = { x: 0.2, y: 0.3, z: 0, w: 1 };
-    // const ADD_SPECIALIST_COLOR = { x: 0.05, y: 0, z: 0.4, w: 0.9 };
-    const ADD_SPECIALIST_COLOR = { x: 0.02, y: 0, z: 0.16, w: 0.9 };
+    const ADD_SPECIALIST_COLOR = { x: 0.05, y: 0, z: 0.4, w: 0.9 };
     const ADD_SPECIALIST_BORDER_COLOR = { x: 0.1, y: 0, z: 0.1, w: 1 };
     this.plotOverlay = overlay.addPlotOverlay();
     this.plotOverlay.addPlots([...validPlots], { fillColor: CITY_TILE_GRAY_COLOR });
@@ -71,12 +70,7 @@ ATIM.decorate = function(overlay) {
         // highlight best plots
         const color = YIELD_COLOR[bestIndex];
         const fillColor = Color.convertToLinear(color);
-        const edgeColor = {
-            x: fillColor.x / 4,
-            y: fillColor.y / 4,
-            z: fillColor.z / 4,
-            w: 1,
-        };
+        const edgeColor = ADD_SPECIALIST_BORDER_COLOR;
         this.plotOverlay.addPlots(bestPlots, { fillColor, edgeColor });
         yields.delete(bestIndex);
     }
