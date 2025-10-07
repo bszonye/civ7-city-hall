1eb3eb131925fd347d1c6482c80ce7a6d728b89f
B. Szonye <bszonye@gmail.com>
2025-10-07 04:34:19 -0700

    City Hall 2.4.0 (#51)
    new Specialist guide
    - City Growth map highlights the largest specialist bonuses
    - color overlays show the best change for each yield type
    - ring highlights show the largest overall change
    - improved Growth panel layout and consistency
    - added Total Yields to growth results

--- ui/interface-modes/bz-interface-mode-acquire-tile.js
+++ ui/interface-modes/bz-interface-mode-acquire-tile.js
@@ -1,20 +1,45 @@
-// import { InterfaceMode } from '/core/ui/interface-modes/interface-modes.js';
+import { InterfaceMode } from '/core/ui/interface-modes/interface-modes.js';
 import { C as ComponentID } from '/core/ui/utilities/utilities-component-id.chunk.js';
 import { C as CityZoomer } from '/base-standard/ui/city-zoomer/city-zoomer.chunk.js';
 import PlotWorkersManager from '/base-standard/ui/plot-workers/plot-workers-manager.js';
 
+const VFX_RING = "VFX_3dUI_Tut_SelectThis_01";
+const VFX_OFFSET = { x: 0, y: 0, z: 0 };
+const VFX_PARAMS = { placement: PlacementMode.TERRAIN };
+
 const YIELD_COLOR = [
-    0xff30b57d,  // #7db530  #80b34d   90° 40 50 green
-    0xff293da3,  //          #a33d29   10° 60 40 red
-    0xff55cef6,  //          #f6ce55   45° 90 65 yellow
-    0xfffe991d,  // #1d99fe  #6ca6e0  210° 65 65 cyan
-    0xffff7b7c,  // #7c7bff  #5c5cd6  240° 60 60 violet
-    0xff2093fd,  // #fd9320  #f5993d   30° 90 60 orange
-    0xffcfb7af,  //          #afb7cf  225° 25 75 gray
+    0x4db380,  // #80b34d   90° 40 50 green
+    0x293da3,  // #a33d29   10° 60 40 red
+    0x55cef6,  // #f6ce55   45° 90 65 yellow
+    0xe0a66c,  // #6ca6e0  210° 65 65 cyan
+    0xff817c,  // #5c5cd6  240° 60 60 violet => #7c81ff
+    0x3d99f5,  // #f5993d   30° 90 60 orange
+    0xcfb7af,  // #afb7cf  225° 25 75 gray
 ];
+const YIELD_COLOR_LINEAR = YIELD_COLOR.map(c => ({
+    ...Color.convertToLinear(c), w: 8/9,
+}));
+const YIELD_BORDER_COLOR_LINEAR = YIELD_COLOR_LINEAR.map(c => ({
+    x: c.x / 4, y: c.y / 4, z: c.z / 4, w: 1,
+}));
 
 // get registered interface mode object
-const ATIM = {};  // InterfaceMode.getInterfaceModeHandler("INTERFACEMODE_ACQUIRE_TILE");
+const ATIM = InterfaceMode.getInterfaceModeHandler("INTERFACEMODE_ACQUIRE_TILE");
+
+// initialize VFX model
+ATIM.growthModelGroup = WorldUI.createModelGroup("bzGrowthModelGroup");
+
+// patch ATIM.undecorate() and ATIM.transitionFrom() to clear VFX
+const ATIM_transitionFrom = ATIM.transitionFrom;
+ATIM.transitionFrom = function transitionFrom(...args) {
+    this.growthModelGroup.clear();
+    return ATIM_transitionFrom.apply(this, args);
+}
+const ATIM_undecorate = ATIM.undecorate;
+ATIM.undecorate = function(...args) {
+    this.growthModelGroup.clear();
+    return ATIM_undecorate.apply(this, args);
+}
 
 // patch ATIM.decorate() to extend its overlay
 ATIM.decorate = function(overlay) {
@@ -39,42 +64,62 @@ ATIM.decorate = function(overlay) {
         fillColor: EXPAND_CITY_COLOR_LINEAR,
         edgeColor: EXPAND_CITY_BORDER_COLOR_LINEAR
     });
-    const yields = new Set([...GameInfo.Yields].map(y => y.$index));
-    const plotYields = new Map();
-    for (const plot of PlotWorkersManager.workablePlotIndexes) {
-        const changes = PlotWorkersManager.bzGetWorkerChanges(plot);
-        plotYields.set(plot, [...changes.plotYields]);
+    // get all workable plots
+    const workablePlots = PlotWorkersManager.workablePlotIndexes.map(plot => {
+        const changes = PlotWorkersManager.bzGetWorkerChanges(plot).plotYields
+            .map((value, color) => ({ color, value }))
+            .filter(y => y.value);
+        const yields = [...changes].sort((a, b) => b.value - a.value);
+        const total = yields.reduce((a, y) => a + y.value, 0);
+        return { plot, total, yields };
+    });
+    const basicPlots = new Set(workablePlots.map(info => info.plot));
+    // highlight the most important plots
+    const usedColors = new Map();
+    const usedPlots = new Set();
+    const highlight = (plot, color) => {
+        this.plotOverlay.addPlots(plot, {
+            fillColor: YIELD_COLOR_LINEAR[color],
+            edgeColor: YIELD_BORDER_COLOR_LINEAR[color],
+        });
+        usedPlots.add(plot);
+        basicPlots.delete(plot);
     }
-    while (yields.size) {
-        // get best change across all plots and yields
-        let bestIndex;
-        let bestChange = 0;
-        for (const changes of plotYields.values()) {
-            for (const index of yields) {
-                const change = changes[index];
-                if (bestChange < change) {
-                    bestIndex = index;
-                    bestChange = change;
-                }
-            }
+    // highlight the best plots for each yield
+    workablePlots.sort((a, b) => {
+        const a1 = a.yields.at(0) ?? { color: GameInfo.Yields.length, value: 0 };
+        const b1 = b.yields.at(0) ?? { color: GameInfo.Yields.length, value: 0 };
+        return b1.value - a1.value || a1.color - b1.color;
+    });
+    for (const info of workablePlots) {
+        if (!info.yields.length) break;
+        const plot = info.plot;
+        const value = info.yields[0].value;  // best yield delta on this plot
+        for (const { color } of info.yields.filter(y => y.value == value)) {
+            // consider all yields tied for best
+            const used = usedColors.get(color);
+            if (used != null && used != value) continue;  // already used
+            usedColors.set(color, value);  // record best value for yield
+            // if multiple yields are valid, only use the first
+            if (!usedPlots.has(plot)) highlight(plot, color);
         }
-        if (!bestChange) break;
-        // get plots matching best change
-        const bestPlots = [];
-        for (const [plot, changes] of plotYields.entries()) {
-            if (changes[bestIndex] == bestChange) {
-                bestPlots.push(plot);
-                plotYields.delete(plot);
-            }
-        }
-        // highlight best plots
-        const color = YIELD_COLOR[bestIndex];
-        const fillColor = Color.convertToLinear(color);
-        const edgeColor = ADD_SPECIALIST_BORDER_COLOR;
-        this.plotOverlay.addPlots(bestPlots, { fillColor, edgeColor });
-        yields.delete(bestIndex);
     }
-    this.plotOverlay.addPlots([...plotYields.keys()], {
+    // highlight the best plots overall
+    const bestTotal = Math.max(0, ...workablePlots.map(({ total }) => total));
+    this.growthModelGroup.clear();
+    for (const info of workablePlots) {
+        if (!bestTotal || !info.yields.length) break;
+        if (info.total != bestTotal) continue;
+        // add a ring highlight to all plots with the best total
+        const plot = info.plot;
+        this.growthModelGroup.addVFXAtPlot(VFX_RING, plot, VFX_OFFSET, VFX_PARAMS);
+        if (usedPlots.has(plot)) continue;
+        // also add a color highlight if the plot isn't already colored
+        const color = info.yields[0].color;
+        highlight(plot, color);
+    }
+    // use basic specialist color for remaining yields
+    this.plotOverlay.addPlots([...basicPlots], {
         fillColor: ADD_SPECIALIST_COLOR,
         edgeColor: ADD_SPECIALIST_BORDER_COLOR,
     });
