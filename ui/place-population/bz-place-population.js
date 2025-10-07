45271b398970c82541ce1f670cee3dbc9b06163f
B. Szonye <bszonye@gmail.com>
2025-10-06 19:10:47 -0700

    add total yields to Add Specialist panel

--- ui/place-population/bz-place-population.js
+++ ui/place-population/bz-place-population.js
@@ -1,66 +1,31 @@
-import { Icon } from '/core/ui/utilities/utilities-image.chunk.js';
 import { D as Databind } from '../../../core/ui/utilities/utilities-core-databinding.chunk.js';
 import { PlacePopulation } from '/base-standard/ui/place-population/model-place-population.js';
-import PlotWorkersManager from '/base-standard/ui/plot-workers/plot-workers-manager.js';
 
-const BASE_ICON = "blp:city_special_base";
-const ARROW_ICON = "blp:adjacencyarrow_east";
-const BONUS_ICON = "blp:city_special_empty";
-Controls.preloadImage(BASE_ICON, "place-population");
-Controls.preloadImage(ARROW_ICON, "place-population");
-Controls.preloadImage(BONUS_ICON, "place-population");
-
-function getYieldHTML(rowIcon, yieldChanges, sorted=false) {
-    const details = [];
-    for (const [i, net] of yieldChanges.entries()) {
-        if (!net) continue;
-        const info = GameInfo.Yields.lookup(i);
-        if (!info) continue;
-        const icon = Icon.getYieldIcon(info.YieldType);
-        const value = Locale.compose("LOC_UI_CITY_DETAILS_YIELD_ONE_DECIMAL", net);
-        details.push({ icon, value });
-    }
-    if (sorted) details.sort((a, b) => b.value - a.value);
-    if (!details.length) return "";
-    const prefix = rowIcon ?
-        `<div class="flex items-center"><img src="${rowIcon}" class="size-5" /></div>` :
-        "";
-    return details.reduce((acc, { icon, value }) => {
-        const margin = acc ? "ml-1\\.5" : "-ml-1";
-        const color = value < 0 ? "text-negative-light" : "text-accent-2";
-        return acc + `<div class="flex items-center ${margin} ${color}"><img src="${icon}" class="size-6" />${value}</div>`;
-    }, prefix);
-}
-function getTotalHTML(totalYields) {
-    const sum = (list) => list.reduce((acc, value) => acc + value);
-    const totalText = Locale.compose(
-        "LOC_UI_CITY_DETAILS_YIELD_ONE_DECIMAL",
-        sum(totalYields)
-    );
-    return Locale.compose("LOC_PLOT_TOTAL_YIELDS", totalText);
+const YIELD_NEUTRAL = "blp:yield_container_neutral";
+const YIELD_POSITIVE = "blp:yield_container_positive";
+const YIELD_NEGATIVE = "blp:yield_container_negative";
+for (const icon of [YIELD_NEUTRAL, YIELD_POSITIVE, YIELD_NEGATIVE]) {
+    Controls.preloadImage(icon, "place-population");
 }
 
-PlacePopulation.bzBaseWorkerYields = "";
-PlacePopulation.bzBaseWorkerMaintenance = "";
-PlacePopulation.bzHoveredWorkerYields = "";
-PlacePopulation.bzHoveredWorkerMaintenance = "";
-PlacePopulation.bzHoveredWorkerTotalSummary = "";
+// initializate PlacePopulation
+PlacePopulation.bzGrowthTitle = "";
+PlacePopulation.bzTotalYieldsText = "";
 
+// patch PlacePopulation.update
 const proto = Object.getPrototypeOf(PlacePopulation);
 const PP_update = proto.update;
 proto.update = function(...args) {
     PP_update.apply(this, args);
-    if (!this.hasHoveredWorkerPlot) return;
-    const plotIndex = this.hoveredPlotWorkerIndex;
-    const changes = PlotWorkersManager.bzGetWorkerChanges(plotIndex);
-    if (!changes) return;
-    this.bzBaseWorkerYields = getYieldHTML(BASE_ICON, changes.baseYields);
-    this.bzBaseWorkerMaintenance = getYieldHTML(BASE_ICON, changes.baseMaintenance);
-    this.bzHoveredWorkerYields = getYieldHTML(ARROW_ICON, changes.plotYields);
-    this.bzHoveredWorkerMaintenance = getYieldHTML(BONUS_ICON, changes.plotMaintenance);
-    const totalYields = changes.netYields.map((a, i) => a + changes.netMaintenance[i]);
-    this.bzHoveredWorkerTotalSummary = getTotalHTML(totalYields);
-    this.bzHoveredWorkerTotalYields = getYieldHTML(null, totalYields, true);
+    this.bzGrowthTitle = this.isTown ? Locale.compose("LOC_UI_TOWN_GROWTH_TITLE") : Locale.compose("LOC_UI_CITY_GROWTH_TITLE");
+    const info = this.hoveredPlotWorkerPlacementInfo;
+    if (!info) return;
+    const sum = (list) => list.reduce((acc, value) => acc + value, 0);
+    const totalYields = sum(info.NextYields) - sum(info.CurrentYields);
+    const totalMaintenance = sum(info.NextMaintenance) - sum(info.CurrentMaintenance);
+    const totalChange = totalYields - totalMaintenance;
+    const total = Locale.compose("LOC_UI_CITY_DETAILS_YIELD_ONE_DECIMAL", totalChange);
+    PlacePopulation.bzTotalYieldsText = Locale.compose("LOC_PLOT_TOTAL_YIELDS", total);
 }
 
 class bzPlacePopulationPanel {
@@ -85,85 +50,46 @@ class bzPlacePopulationPanel {
         }
     }
     beforeAttach() { }
-    afterAttach() { }
+    afterAttach() {
+        const c = this.component;
+        const header = c.subsystemFrame.querySelector("fxs-header");
+        Databind.attribute(header, "title", "g_PlacePopulation.bzGrowthTitle");
+    }
     beforeDetach() { }
     afterDetach() { }
     afterBuildSpecialistInfo() {
-        // improve Breakdown layout
         const c = this.component;
-        const heads = c.specialistMaximizedContainer.querySelectorAll(
-            [
-                `[data-l10n-id="LOC_BUILDING_PLACEMENT_TILE_TYPE"]`,
-                `[data-l10n-id="LOC_BUILDING_PLACEMENT_SPECIALIST_BONUS"]`,
-                `[data-l10n-id="LOC_BUILDING_PLACEMENT_SPECIALIST_MAINTENANCE"]`,
-            ].join(",")
-        );
-        for (const head of heads) {
-            const section = head.parentElement;
-            section.classList.remove("mx-2");
-            section.classList.add("flex-wrap", "mt-1", "mx-1");
-            for (const text of section.querySelectorAll(".text-sm")) {
-                // shrink and align text
-                text.parentElement.classList.add("items-center");
-                text.classList.remove("text-sm");
-                text.classList.add("text-xs");
+        const views = [c.specialistMinimizedContainer, c.specialistMaximizedContainer];
+        for (const view of views) {
+            // Results: add total changes
+            const resultsHeading = view.querySelector(
+                `[data-l10n-id="LOC_BUILDING_PLACEMENT_RESULTS"]`
+            );
+            const results = resultsHeading.parentElement;
+            const totals = document.createElement("div");
+            totals.className = "self-center text-sm -mt-1 mb-2";
+            Databind.loc(totals, "{{g_PlacePopulation.bzTotalYieldsText}}");
+            results.insertAdjacentElement("afterend", totals);
+            // Breakdown: improve layout
+            const breakdownHeadings = view.querySelectorAll(
+                [
+                    `[data-l10n-id="LOC_BUILDING_PLACEMENT_TILE_TYPE"]`,
+                    `[data-l10n-id="LOC_BUILDING_PLACEMENT_SPECIALIST_BONUS"]`,
+                    `[data-l10n-id="LOC_BUILDING_PLACEMENT_SPECIALIST_MAINTENANCE"]`,
+                ].join(",")
+            );
+            for (const head of breakdownHeadings) {
+                const section = head.parentElement;
+                section.classList.remove("mx-2");
+                section.classList.add("flex-wrap", "mt-1", "mx-1");
+                for (const text of section.querySelectorAll(".text-sm")) {
+                    // shrink and align text
+                    text.parentElement.classList.add("items-center");
+                    text.classList.remove("text-sm");
+                    text.classList.add("text-xs");
+                }
             }
         }
-    }
-    TODOafterBuildSpecialistInfo(specialistInfoFrame) {  // TODO: remove
-        // fix styling
-        const infoContainer = specialistInfoFrame.lastChild;
-        infoContainer.classList.remove("ml-5", "ml-8");
-        infoContainer.classList.add("self-center");
-        const iconElement = infoContainer.firstChild;
-        iconElement.classList.add("img-add_specialists_icon");
-        const textContainer = infoContainer.lastChild;
-        textContainer.classList.remove("ml-2", "max-w-84");
-        textContainer.classList.add("ml-4", "w-84");
-        // add yield changes
-        const changesContainer = document.createElement("div");
-        const yieldHeader = document.createElement("div");
-        yieldHeader.className = "text-secondary mt-2 mb-0\\.5";
-        yieldHeader.setAttribute("data-l10n-id", "LOC_BUILDING_PLACEMENT_SPECIALIST_BONUS");
-        changesContainer.appendChild(yieldHeader);
-        // base yield changes
-        const baseYields = document.createElement("div");
-        baseYields.className = "flex text-xs";
-        Databind.html(baseYields, "{{g_PlacePopulation.bzBaseWorkerYields}}");
-        changesContainer.appendChild(baseYields);
-        // plot yield changes
-        const plotYields = document.createElement("div");
-        plotYields.className = "flex text-xs";
-        Databind.html(plotYields, "{{g_PlacePopulation.bzHoveredWorkerYields}}");
-        changesContainer.appendChild(plotYields);
-        // add maintenance changes
-        const maintenanceHeader = document.createElement("div");
-        maintenanceHeader.className = "text-secondary mt-2 mb-0\\.5";
-        maintenanceHeader.setAttribute("data-l10n-id", "LOC_BUILDING_PLACEMENT_SPECIALIST_MAINTENANCE");
-        changesContainer.appendChild(maintenanceHeader);
-        // base maintenance
-        const baseMaintenance = document.createElement("div");
-        baseMaintenance.className = "flex text-xs";
-        Databind.html(baseMaintenance, "{{g_PlacePopulation.bzBaseWorkerMaintenance}}");
-        changesContainer.appendChild(baseMaintenance);
-        // maintenance discounts
-        const plotMaintenance = document.createElement("div");
-        plotMaintenance.className = "flex text-xs";
-        Databind.html(plotMaintenance, "{{g_PlacePopulation.bzHoveredWorkerMaintenance}}");
-        changesContainer.appendChild(plotMaintenance);
-        // total yield summary
-        const workerTotals = document.createElement("div");
-        workerTotals.className = "text-secondary mt-2";
-        Databind.html(workerTotals, "{{g_PlacePopulation.bzHoveredWorkerTotalSummary}}");
-        changesContainer.appendChild(workerTotals);
-        // total yield details
-        const totalYields = document.createElement("div");
-        totalYields.className = "flex text-xs";
-        Databind.html(totalYields, "{{g_PlacePopulation.bzHoveredWorkerTotalYields}}");
-        changesContainer.appendChild(totalYields);
-        // wrap up
-        textContainer.appendChild(changesContainer);
-        return specialistInfoFrame;
     }
 }
 Controls.decorate("panel-place-population", (c) => new bzPlacePopulationPanel(c));
