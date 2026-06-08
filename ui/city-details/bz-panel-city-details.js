e1eb61847bd9a9087075c23d0f4e400f64985dc9
B. Szonye <bszonye@gmail.com>
2026-06-08 15:25:31 -0700

    fix City Details arrows

--- ui/city-details/bz-panel-city-details.js
+++ ui/city-details/bz-panel-city-details.js
@@ -155,11 +155,11 @@ const BZ_HEAD_STYLE = [
 }
 .bz-city-hall .panel-city-details.bz-nav-help .bz-cycle-city {
     top: 0.6666666667rem;
-    left: 1rem;
+    left: 0.2777777778rem;
 }
 .bz-city-hall .panel-city-details.bz-no-help .bz-cycle-city {
     top: 0.6111111111rem;
-    left: 1rem;
+    left: 0;
 }
 .bz-overview-entry.bz-odd-row {
     background-color: ${BZ_COLOR.bronze6}99;
